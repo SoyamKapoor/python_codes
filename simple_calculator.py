8e40a95263a4054af4a0ba8b9ccd097d02717cae
Soyam Kapoor <kapoorsoyam4@gmail.com>
2024-06-11 15:48:37 +0500

    Initial commit

---
+++ simple_calculator.py
@@ -0,0 +1,7 @@
+#SIMPLE CALCULATOR WITHOUT USER INPUT DEVELOPED BY SOYAM KAPOOR
+print("<=========================SIMPLE CALCULATOR WITHOUT USING USER INPUT=========================>")
+print("Addition         => 4 + 5 = ",4+5)
+print("Subtraction      => 4 - 5 = ",4-5)
+print("Multiplication   => 4 * 5 = ",4*5)
+print("Division         => 4 / 5 = ",4/5)
+print("Modulus          => 4 % 5 = ",4%5)
